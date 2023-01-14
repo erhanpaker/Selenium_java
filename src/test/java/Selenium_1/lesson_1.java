9c4cf07548e46e04fc392381456e5572ceb94c67
erhan paker <erhanpaker@gmail.com>
2023-01-14 14:01:17 +0300

    version 2

--- src/test/java/Selenium_1/lesson_1.java
+++ src/test/java/Selenium_1/lesson_1.java
@@ -1,7 +1,15 @@
 package Selenium_1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class lesson_1 {
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.amazon.com");
 
     }
 }
