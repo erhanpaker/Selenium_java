29d165b06d72474c33038d25f4dce54a6f1d199d
erhan paker <erhanpaker@gmail.com>
2023-01-15 16:48:43 +0300

    version 1

---
+++ src/test/java/selenium_4/lesson_1.java
@@ -0,0 +1,21 @@
+package selenium_4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class lesson_1 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://amazon.com");
+
+    }
+}
