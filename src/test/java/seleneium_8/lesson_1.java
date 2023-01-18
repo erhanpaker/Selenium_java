c38a754e0b6348da331dd4ef328ed6171479673f
erhan paker <erhanpaker@gmail.com>
2023-01-18 16:05:17 +0300

    version 1.2.2

--- src/test/java/seleneium_8/lesson_1.java
+++ src/test/java/seleneium_8/lesson_1.java
@@ -24,26 +24,13 @@ public class lesson_1 {
 
         driver.get("https://automationexercise.com/");
 
-        WebElement logoelementi = driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
-        Assert.assertTrue(logoelementi.isDisplayed());
+        WebElement login = driver.findElement(By.xpath("//img[@src='/static/images/home/logo.png']"));
+        Assert.assertTrue(login.isDisplayed());
 
-        WebElement login = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a"));
-        login.click();
 
-        WebElement yazi = driver.findElement(By.xpath("//h2[text()= 'Login to your account']"));
-        Assert.assertTrue(yazi.isDisplayed());
 
-        WebElement adres =driver.findElement(By.xpath("//input[@data-qa= 'login-email']"));
-        adres.sendKeys("erhanpaker@gmail.com");
 
-        WebElement password = driver.findElement(By.xpath("//input[@data-qa= 'login-password']"));
-        password.sendKeys("12345");
 
-        WebElement buton = driver.findElement(By.xpath("//button[@data-qa= 'login-button']"));
-        buton.click();
-
-        WebElement log = driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[10]/a/text()"));
-        Assert.assertTrue(log.isDisplayed());
 
 
 
